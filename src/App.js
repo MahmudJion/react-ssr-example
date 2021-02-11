dd991a69203a4c12e6aaf4a645d44ec59b57c45b
MahmudJion <mahmudh.jion02@gmail.com>
2021-02-11 17:31:03 +0600

    updated on app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is an example of server side rendering</h1>
+        <h1>This line shows in server side rendering</h1>
       </header>
     </div>
   );
